a1facb409dbe68065ef914c58f98d866d2bbceb8
acferten <gowiththeirname@gmail.com>
2022-09-26 15:52:49 +0700

    Выход, создание заявки на разработку дизайна интерьера помещения, просмотр своих заявок, удаление своей заявки сделано

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
+from django.urls import include
 from django.views.generic import RedirectView
 
 from . import views
 
-
 urlpatterns = [
-    path('', RedirectView.as_view(url='/home/', permanent=True)),
-    path('signup/', views.signup, name='signup'),
-    path('home/', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
-    path('myorders/create/', views.ordercreate, name='order-create'),
-    path('myorders/', views.OrdersByUserListView.as_view(), name='myorders'),
-]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', RedirectView.as_view(url='/home/', permanent=True)),
+                  path('signup/', views.signup, name='signup'),
+                  path('home/', views.index, name='index'),
+                  path('profile/', views.profile, name='profile'),
+                  path('myorders/create/', views.ordercreate, name='order-create'),
+                  path('myorders/', views.OrdersByUserListView.as_view(), name='myorders'),
+                  path('order/<int:pk>', views.OrderDetailView.as_view(), name='order-detail'),
+                  path('order/<int:pk>/delete', views.OrderDeleteView.as_view(), name='order-delete'),
+                  path('accounts/', include('django.contrib.auth.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import request
+from django.http import request, Http404
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .filters import OrderFilter
@@ -46,11 +47,12 @@ def ordercreate(request):
     if request.method == 'POST':
         form = NewOrderForm(request.POST, request.FILES)
         if form.is_valid():
-            # form.save()
             stock = form.save(commit=False)
             stock.orderer = request.user
             stock.status = 'н'
+            stock.category = stock.category
             stock.save()
+            form.save_m2m()
             return redirect('myorders')
     else:
         form = NewOrderForm()
@@ -62,7 +64,57 @@ class OrdersByUserListView(LoginRequiredMixin, generic.ListView):
     template_name = 'user_orders.html'
     paginate_by = 5
 
+    def get_queryset(self):
+        return Order.objects.filter(orderer=self.request.user).order_by('date')
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs,)
+        context = super().get_context_data(**kwargs, )
         context['filter'] = OrderFilter(self.request.GET, queryset=self.get_queryset())
         return context
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+
+class OrderDeleteView(generic.DeleteView):
+    model = Order
+    success_url = reverse_lazy('myorders')
+    template_name = 'delete_confirm.html'
+
+    def get_object(self, queryset=None):
+        """
+        Check the logged in user is the owner of the object or 404
+        """
+        obj = super(OrderDeleteView, self).get_object(queryset)
+        if obj.orderer != self.request.user:
+            raise Http404(
+                "You don't own this object"
+            )
+        return obj
+
+# class DeleteCar(DeleteView):
+#     model = Car
+#     success_url = reverse_lazy('c2crental:list_user_cars')
+#     template_name = 'c2crental/car/delete_confirm_car.html'
+#     success_message = _("Car has been deleted.")
+#
+#     def delete(self, request, *args, **kwargs):
+#         messages.success(self.request, self.success_message)
+#         return super(DeleteCar, self).delete(request, *args, **kwargs)
+#
+#     def get_queryset(self):
+#         owner = self.request.user
+#         return self.model.objects.filter(owner=owner)
+#
+#     def get_object(self, queryset=None):
+#         """
+#         Check the logged in user is the owner of the object or 404
+#         """
+#         obj = super(MyView, self).get_object(queryset)
+#         if obj.owner != self.request.user:
+#             raise Http404(
+#                 _("You don't own this object")
+#             )
+#         return obj
